e1f5375db54933084d547a16a92ada77714ab25e
Ross Gardler <rgardler@opendirective.com>
2012-12-31 01:00:22 +0000

    add a build script to ease deployment to different clients

---
+++ build.bat
@@ -0,0 +1,12 @@
+echo off
+rem ----- use the location of this script to infer $FLORENCE_HOME -------
+set FLORENCE_HOME=%~dp0..
+if "%WOOKIE_HOME%"=="" set WOOKIE_HOME=%~dp0..\wookie
+
+if "%1"=="" set DEPLOY_DIR=%FLORENCE_HOME%\florence_win8\JS_Client\widget
+if "%1"=="win8" set DEPLOY_DIR=%FLORENCE_HOME%\florence_win8\JS_Client\widget
+if "%1"=="win8_c#" set DEPLOY_DIR=%FLORENCE_HOME%\florence_win8\Client\widget
+
+if "%1"=="wookie" set DEPLOY_DIR=%WOOKIE_HOME%\build\webapp\wookie\deploy
+
+ant generate-all-widgets -Dwidget.deploy.dir=%deploy_dir%
