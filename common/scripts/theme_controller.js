41147ee3797c571552f586f98e4861b57ae0bab2
Ross Gardler <rgardler@opendirective.com>
2013-02-19 01:39:09 +0000

    respond to the themeName setting by setting the theme appropriately

--- common/scripts/theme_controller.js
+++ common/scripts/theme_controller.js
@@ -1,15 +1,14 @@
 var ${widget.shortname}_theme_controller = {
     init:function() {
-        // FIXME: theme should be stored in a setting and loaded dynamically
-        if (Math.random() > .5) {
-            window.widget.views.switchTheme("light");
-        } else {
+        if (window.widget.preferences.getItem("themeName") == "dark") {
             window.widget.views.switchTheme("dark");
+        } else {
+            window.widget.views.switchTheme("light");
         };
     },
 };
 
 
-$(document).on('pageinit', '#home', function (event) {
+$(document).on('pagebeforeshow', '#home', function (event) {
     ${widget.shortname}_theme_controller.init();
 });
